1f82c542ba55d9cf62f6bad9202de1624278f098
leentaylor <leentaylor@gmail.com>
2023-11-05 15:38:46 +1100

    autoformatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 // Import everything needed to use the `useQuery` hook
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from "@apollo/client";
 
 import React, { useState, useEffect } from "react";
 import { getStuff } from "./regularAsyncModule";
@@ -8,7 +7,7 @@ import { loadXHR, blobToBase64, sleep } from "./helpers";
 
 const GET_WARRIOR = gql`
   query GetWarrior($id: String!) {
-    warrior(id: $id) {
+    warrior() {
       id
       name
       data
@@ -16,23 +15,20 @@ const GET_WARRIOR = gql`
   }
 `;
 
-
 function App() {
   const [warriorFilter, setWarriorFilter] = useState("");
 
   const [base64ImageProcessed, setBase64ImageProcessed] = useState("");
   const [base64ImageOriginal, setBase64ImageOriginal] = useState("");
 
-
-  const { loading, error, data} = useQuery(GET_WARRIOR, {variables: {id: warriorFilter}});
-
+  const { loading, error, data } = useQuery(GET_WARRIOR, {
+    variables: { id: warriorFilter },
+  });
 
   useEffect(() => {
-    console.log("I fire when data changes")
-    console.log(data)
+    console.log("I fire when data changes");
+    console.log(data);
   }, [data]);
-
-
 
   useEffect(() => {
     const doProcessing = async () => {
@@ -61,26 +57,20 @@ function App() {
 
   return (
     <div className="App">
-      
+      {loading && <p>Loading</p>}
 
-      {loading && (<p>Loading</p>)}
-      
-      {error && (<p>Error! {error.message}</p>)}
-
+      {error && <p>Error! {error.message}</p>}
 
       {data?.warrior && (
-      <div key={data.warrior.id}>
-        <h3>{data.warrior.name}</h3>
-        <img src={data.warrior.data} className="App-logo" alt="logo" />
-     </div>
+        <div key={data.warrior.id}>
+          <h3>{data.warrior.name}</h3>
+          <img src={data.warrior.data} className="App-logo" alt="logo" />
+        </div>
       )}
 
-
-<p>
-          <button onClick={() => setWarriorFilter("001")}>filter warrior</button>
-        </p>
-
-
+      <p>
+        <button onClick={() => setWarriorFilter("001")}>filter warrior</button>
+      </p>
 
       <header className="App-header">
         {base64ImageOriginal && !base64ImageProcessed && (
