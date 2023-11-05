1d822c0fdc8fabc5d3a1424635620b06590fd511
leentaylor <leentaylor@gmail.com>
2023-11-05 15:40:01 +1100

    remove unnecessary code
    # Conflicts:
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { useQuery, gql } from "@apollo/client";
 
 import React, { useState, useEffect } from "react";
-import { getStuff } from "./regularAsyncModule";
 import { loadXHR, blobToBase64, sleep } from "./helpers";
 
 const GET_WARRIOR = gql`
@@ -16,40 +15,28 @@ const GET_WARRIOR = gql`
 `;
 
 function App() {
-  const [warriorFilter, setWarriorFilter] = useState("");
 
   const [base64ImageProcessed, setBase64ImageProcessed] = useState("");
   const [base64ImageOriginal, setBase64ImageOriginal] = useState("");
 
-  const { loading, error, data } = useQuery(GET_WARRIOR, {
-    variables: { id: warriorFilter },
-  });
+
+  const { loading, error, data} = useQuery(GET_CARD, {variables: {id: base64ImageOriginal ? "001" : ""}});
 
   useEffect(() => {
-    console.log("I fire when data changes");
-    console.log(data);
-  }, [data]);
-
-  useEffect(() => {
-    const doProcessing = async () => {
-      console.log("the original is ready for processing");
-      const result = await getStuff(base64ImageOriginal);
-      setBase64ImageProcessed(result);
-    };
-
-    if (base64ImageOriginal) {
-      doProcessing();
+    console.log("I fire when data returned from the graphql hook changes")
+    if (data?.card)
+    {
+      console.log("data from graph ql has given me an image")
+      setBase64ImageProcessed(data.card.data)
     }
-  }, [base64ImageOriginal]);
+  }, [data]);
 
   const clickMe = async () => {
     console.log("I will load the original");
 
     await sleep(1000);
-
     const myBlob = await loadXHR("/creditcard.png");
     const myBase64 = await blobToBase64(myBlob);
-
     console.log("now I have loaded the original");
 
     setBase64ImageOriginal(myBase64);
@@ -57,20 +44,9 @@ function App() {
 
   return (
     <div className="App">
-      {loading && <p>Loading</p>}
-
-      {error && <p>Error! {error.message}</p>}
-
-      {data?.warrior && (
-        <div key={data.warrior.id}>
-          <h3>{data.warrior.name}</h3>
-          <img src={data.warrior.data} className="App-logo" alt="logo" />
-        </div>
-      )}
-
-      <p>
-        <button onClick={() => setWarriorFilter("001")}>filter warrior</button>
-      </p>
+      
+      {loading && (<p>Loading</p>)}
+      {error && (<p>Error! {error.message}</p>)}
 
       <header className="App-header">
         {base64ImageOriginal && !base64ImageProcessed && (
